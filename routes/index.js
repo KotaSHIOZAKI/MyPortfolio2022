e7763a3a06e3d2c20031f1faf81e6ca4d92fc494
KotaSHIOZAKI <ksw2270274@stu.o-hara.ac.jp>
2022-06-10 03:31:40 +0000

    必要なページの構築

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {});
+  var data = {
+    file: 'index'
+  }
+  res.render('base', data);
 });
 router.get('/skills', function(req, res, next) {
-  res.render('index', {});
+  var data = {
+    file: 'skills'
+  }
+  res.render('base', data);
 });
 router.get('/occupation', function(req, res, next) {
-  res.render('index', {});
+  var data = {
+    file: 'occupation'
+  }
+  res.render('base', data);
 });
 
 module.exports = router;
